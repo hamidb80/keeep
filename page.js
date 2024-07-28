f29ddd8d51dac1a7c3441c7d03b1f38413b7f19a
hamidb80 <hr.bolouri@gmail.com>
2024-07-28 15:02:11 +0330

    better

--- page.js
+++ page.js
@@ -1,5 +1,24 @@
-function autoheight(element) {
+function q(sel) {
+  // alias
+  return document.querySelector(sel)
+}
+
+
+function debounced(fn, delay) {
+  let timeoutId
+  return function (...args) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn.apply(this, args), delay)
+  }
+}
+
+function autoHeight(element) {
   // https://stackoverflow.com/questions/55811423/css-remove-scroll-bar-and-replace-with-variable-height-for-textarea-in-a-table
   element.style.height = "0px"
   element.style.height = element.scrollHeight + "px"
+}
+
+function onTextAreaInput(element) {
+  autoHeight(element)
+  q('#md-result').innerHTML = marked.parse(element.value)
 }
