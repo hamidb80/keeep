86b615f5fd23fb6d60bc235799e83145ab1e7694
hamidb80 <hr.bolouri@gmail.com>
2024-08-07 12:07:15 +0330

    .

--- src/backend/main.nim
+++ src/backend/main.nim
@@ -8,3 +8,32 @@ proc discover: seq[Path] =
 
 
 # parse
+
+
+find "<article ..."
+find "<tags ..."
+
+find "<time" if not, fill time
+
+find title inside rticles
+
+configurable templates
+config file for base_url, site_name, ...
+
+
+page:
+  about
+  settings
+    name
+    export local DB
+    import DB
+
+  notes table:
+    - different formuals for scoring
+    - searchable
+    - show name, tag, time, score
+
+  note view:
+    - content
+    - buttuns for remembering
+  
