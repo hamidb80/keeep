c66ac1a52f5e0efb6a54a6e95bebbc9caf50d624
hamidb80 <hr.bolouri@gmail.com>
2024-08-07 20:44:50 +0330

    blueprint

--- src/backend/main.nim
+++ src/backend/main.nim
@@ -1,39 +1,145 @@
-import std/[xmlparser, xmltree, strtabs, os, strutils]
+import std/[
+  xmltree, 
+  strutils, 
+  times,
+  os, paths, streams,
+  algorithm]
+
+import pkg/htmlparser
+
+# ---------------------------------------
+
+type
+  HashTag* = object
+    ## simple tag: #math
+    ## data   tag: #page: 2
+    name*, value*: string
 
 
-proc discover: seq[Path] = 
-  for f in walkDirRec "./":
+  Note* = object
+    timestamp*: Datetime
+    # title*: string
+    content*: XmlNode
+    hashtags*: seq[HashTag]
+
+
+using 
+  x: XmlNode
+  p: Path
+  s: string
+
+# ---------------------------------------
+
+template `<<`(smth): untyped {.dirty.} =
+  result.add smth
+
+template raisev(msg): untyped {.dirty.} =
+  raise newException(ValueError, msg)
+
+# ---------------------------------------
+
+func isElement(x): bool = 
+  x.kind == xnElement
+
+
+func tagLabel(s): string = 
+  if   s.len <  2 : raisev "tag cannot have length of < 2"
+  elif s[0] != '#': raisev "tag should start with #"
+  else            : s.substr 1
+
+func parseHashTag(s): HashTag = 
+  let  parts    = s.split(':', 1)
+  case parts.len
+  of 1: HashTag(name: tagLabel parts[0])
+  of 2: HashTag(name: tagLabel parts[0], value: parts[1])
+  else: raisev "cannot happen"
+
+func parseHashTags(s): seq[HashTag] = 
+  for l in splitLines s:
+    if not isEmptyOrWhitespace l:
+      <<   parseHashTag  strip l
+
+
+proc discover(dir: Path): seq[Path] = 
+  for f in walkDirRec $dir:
     if f.endsWith ".html":
-      add htmlFiles, f
+      << Path f
+
+proc parseHtmlFromFile(p): XmlNode = 
+  parseHtml newFileStream($p, fmRead)
 
 
-# parse
+func dfs(x; visit: proc(n: XmlNode): bool) {.effectsOf: visit.} = 
+  if x.visit and x.isElement:
+    for ent in x:
+      dfs ent, visit
 
-
-find "<article ..."
-find "<tags ..."
-
-find "<time" if not, fill time
-
-find title inside rticles
-
-configurable templates
-config file for base_url, site_name, ...
-
-
-page:
-  about
-  settings
-    name
-    export local DB
-    import DB
-
-  notes table:
-    - different formuals for scoring
-    - searchable
-    - show name, tag, time, score
-
-  note view:
-    - content
-    - buttuns for remembering
+func findTitle(x): string = 
+  var titles: seq[XmlNode]
   
+  proc visit(n: XmlNode): bool = 
+    if n.isElement and n.tag in "h1 h2 h3 h4 h5 h6":
+      titles.add n
+    true
+
+  func tagCmp(a, b: XmlNode): int = 
+    cmp a.tag[1], b.tag[1] # only compare number part - i.e. h[1] vs h[3]
+  
+  dfs x, visit
+  sort titles, tagCmp, Ascending
+  
+  case  titles.len 
+  of 0: raisev "cannot find any header tag"
+  else: titles[0].innerText
+
+
+func initNode(html: sink XmlNode): Note =
+  case html.tag
+  of "document": # automatically generated tag for wrapping
+    for el in html:
+      if el.kind == xnElement:
+        case el.tag
+        of "article": 
+          result.content = el
+        
+        of "tags":
+          result.hashtags = el.innerText.parseHashTags
+        
+        of "time": 
+          discard
+        
+        of "data": 
+          discard
+        
+        else: 
+          raisev "invalid tag in direct child of html file: " & el.tag
+  
+  else:
+    raisev "the note should have at least these tags at the root: artice, tags"
+
+when false:
+  block config:
+    configurable templates
+    config file "for base_url, site_name"
+
+  block pages:
+    about
+    settings:
+      name
+      export local DB
+      import DB
+
+    notes table:
+      different formuals forr scoring
+      searchable
+      show name, tag, time, score
+
+    note view:
+      content
+      buttuns forr remembering
+
+
+when isMainModule:
+  for p in discover Path "./notes":
+    let html = parseHtmlFromFile p
+    echo initNode html
