dfc9c4432b0603a976d1359ba66d4851064e5879
hamidb80 <hr.bolouri@gmail.com>
2024-08-16 15:15:30 +0330

    add summary for tests

--- src/browser/page.js
+++ src/browser/page.js
@@ -1,5 +1,27 @@
 // Utils ---------------------------------------------
 
+function dedent(text) {
+  // removes common indentation from `text`
+  const lines = text.split('\n')
+
+  let minIndent = Infinity
+  lines.forEach(line => {
+    if (line.trim().length != 0) {
+      const match = line.match(/^\s*/)
+      if (match && match[0].length < minIndent) {
+        minIndent = match[0].length
+      }
+    }
+  })
+
+  // Find the minimum indentation among all lines
+
+  return lines
+    .map(line => line.substring(minIndent)) // crop after minIndent
+    .join('\n')
+    .trim()
+}
+
 function p2(smth) {
   return String(smth).padStart(2, '0')
 }
@@ -412,4 +434,9 @@ up.compiler('latex', el => {
 
   el.replaceChildren(bdo)
   katex.render(tex, bdo, opts)
+})
+
+up.compiler('[highlight]', element => {
+  element.innerHTML = dedent(element.innerHTML)
+  hljs.highlightElement(element)
 })
